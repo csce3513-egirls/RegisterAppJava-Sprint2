7586fc10cf7f134a47c370b72a6d62e1e045c212
David <clairmont.david@gmail.com>
2020-04-09 18:25:08 -0500

    changed TransactionDetail Routing

--- src/main/java/edu/uark/registerapp/controllers/TransactionDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionDetailRouteController.java
@@ -1,44 +1,40 @@
 package edu.uark.registerapp.controllers;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.controllers.enums.QueryParameterNames;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.models.api.Product;
-import edu.uark.registerapp.models.entities.ActiveUserEntity;
-import edu.uark.registerapp.models.enums.EmployeeClassification;
 
 @Controller
 @RequestMapping(value = "/transactionDetail")
 public class TransactionDetailRouteController extends BaseRouteController {
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView start(
-        @RequestParam final Map<String, String> queryParameters,
-		final HttpServletRequest request
-    ) {
-        
-        final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
-        if(!activeUserEntity.isPresent()){
-            return this.buildInvalidSessionResponse();
-        } 
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showTransaction(
+		@RequestParam final Map<String, String> queryParameters
+	) {
 
-        
+		try {
+		} catch (NotFoundException e) {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.TRANSACTION_DETAIL.getRoute()));
+		}
 
-        return null;
-    }
-    
+		ModelAndView modelAndView =
+				new ModelAndView(ViewNames.TRANSACTION_DETAIL.getViewName());
+
+		return modelAndView;
+	}
 }
