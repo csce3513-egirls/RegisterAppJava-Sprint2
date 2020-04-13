de177d3307d110c3cf25e3678ec05a75c9ef2210
samuel-witucki <samuel.witucki.p@gmail.com>
2020-04-13 17:14:49 -0500

    Added {transactionId} route to TransactionDetailRouteController

--- src/main/java/edu/uark/registerapp/controllers/TransactionDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionDetailRouteController.java
@@ -2,12 +2,14 @@ package edu.uark.registerapp.controllers;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +24,7 @@ import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.models.entities.TransactionEntity;
 import edu.uark.registerapp.models.enums.EmployeeClassification;
 import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.commands.transactions.TransactionQuery;
 
 @Controller
 @RequestMapping(value = "/transactionDetail")
@@ -59,6 +62,46 @@ public class TransactionDetailRouteController extends BaseRouteController {
             }
         return modelAndView;
     }
+
+    @RequestMapping(value = "{transactionId}", method = RequestMethod.GET)
+    public ModelAndView startWithTransaction(
+        @PathVariable final UUID transactionId,
+        @RequestParam final Map<String, String> queryParameters,
+        final HttpServletRequest request
+    ) {
+        final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+        }
+        
+        final ModelAndView modelAndView = 
+            this.setErrorMessageFromQueryString(
+                new ModelAndView(ViewNames.
+                TRANSACTION_DETAIL.getViewName()),
+                queryParameters);
+                
+        //TODO: Does this required elevated user status like in ProductDetailRouteController?
+
+        try {
+            modelAndView.addObject(
+                ViewModelNames.TRANSACTION.getValue(),
+				this.transactionQuery.setTransactionId(transactionId).execute());
+        } catch (final Exception e) {
+            modelAndView.addObject(
+                ViewModelNames.ERROR_MESSAGE.getValue(),
+                e.getMessage());
+            modelAndView.addObject(
+                ViewModelNames.TRANSACTION.getValue());
+                ///TODO: Another parameter required?
+        }
+        return modelAndView;
+    }
+
+
+    @Autowired
+    private TransactionQuery transactionQuery;
+
     @Autowired
 	private ProductsQuery productsQuery;
 }
