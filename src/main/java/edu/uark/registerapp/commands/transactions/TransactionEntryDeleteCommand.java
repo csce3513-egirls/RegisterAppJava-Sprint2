dc77bc1efbc19f8fa8e74b9c0bf6b52682428f6d
David <clairmont.david@gmail.com>
2020-04-13 16:38:16 -0500

    finished TransactionEntryDeleteCommand

--- src/main/java/edu/uark/registerapp/commands/transactions/TransactionEntryDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/transactions/TransactionEntryDeleteCommand.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.entities.TransactionEntryEntity;
 import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
-import javassist.NotFoundException;
 
 @Service
 public class TransactionEntryDeleteCommand implements VoidCommandInterface {
