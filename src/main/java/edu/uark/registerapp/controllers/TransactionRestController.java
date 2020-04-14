aee65d0984caf78645c2e435c1b201770e5d4da2
samuel-witucki <samuel.witucki.p@gmail.com>
2020-04-14 10:25:13 -0500

    Modified TransactionRestController to allow for transaction deletion

--- src/main/java/edu/uark/registerapp/controllers/TransactionRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRestController.java
@@ -20,6 +20,7 @@ import edu.uark.registerapp.commands.products.ProductUpdateCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.commands.transactions.TransactionDeleteCommand;
 
 // Added mapping and api response so shopping cart image can redirect
 // to transactionDetail.html
@@ -34,5 +35,20 @@ public class TransactionRestController extends BaseRestController{
 			.setRedirectUrl(ViewNames.TRANSACTION_DETAIL.getRoute());
     }
 
+    @RequestMapping(value = "/deleteTransaction", method = RequestMethod.DELETE)
+    public @ResponseBody ApiResponse deleteTransaction(
+        @PathVariable final UUID transactionId, //TODO: make sure this works for deleting transaction
+        final HttpServletRequest request
+    ) {
+        this.transactionDeleteCommand.
+            setTransactionId(transactionId).
+            execute();
+
+        return (new ApiResponse())
+		    .setRedirectUrl(ViewNames.MAIN_MENU.getRoute());
+    }
+
     
+    @Autowired
+    private TransactionDeleteCommand transactionDeleteCommand;
 }
