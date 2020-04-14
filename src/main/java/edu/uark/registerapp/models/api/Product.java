f4b6f4181e3a7d088017293e4d985858841bd65d
kmalas <60142249+kmalas@users.noreply.github.com>
2020-04-13 22:28:41 -0500

    Added price
    Added price to Product.java and allow for edits via the productDetail view

--- src/main/java/edu/uark/registerapp/models/api/Product.java
+++ src/main/java/edu/uark/registerapp/models/api/Product.java
@@ -40,6 +40,17 @@ public class Product extends ApiResponse {
 		return this;
 	}
 
+	private long price;
+
+	public long getPrice() {
+		return this.price;
+	}
+
+	public Product setPrice(final long price) {
+		this.price = price;
+		return this;
+	}
+
 	private String createdOn;
 
 	public String getCreatedOn() {
@@ -62,6 +73,7 @@ public class Product extends ApiResponse {
 		super();
 
 		this.count = -1;
+		this.price = 0L;
 		this.id = new UUID(0, 0);
 		this.lookupCode = StringUtils.EMPTY;
 
@@ -74,6 +86,7 @@ public class Product extends ApiResponse {
 		this.id = productEntity.getId();
 		this.count = productEntity.getCount();
 		this.lookupCode = productEntity.getLookupCode();
+		this.price = productEntity.getPrice();
 
 		this.setCreatedOn(productEntity.getCreatedOn());
 	}
